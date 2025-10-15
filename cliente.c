53cbd692e9bb552e4e517252bf408bd5be0d2fd9
NicolasDPZ <nicolas.perdigonz@javeriana.edu.co>
2025-10-14 19:23:43 -0500

    ¨entrega¨

--- cliente.c
+++ cliente.c
@@ -1,3 +1,11 @@
+/**************************************************************************************
+ * Fecha: 14/10/2025
+ * Autor: Nicolas David Perdigon Zambrano
+ * Materia: Sistemas Operativos
+ * Tema: lab Productor-Consumidor
+ * profesor: John Corredor
+ * ************************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,12 +16,12 @@
 #define FIFO_FILE "/tmp/fifo_twoway"//define la ruta del fifo para comunicacion
 
 int main() {//inicia el programa principal o main
-   int fd;		//
-   int end_process;	//
-   int stringlen;	//
-   int read_bytes;	//
-   char readbuf[80];	//
-   char end_str[5];	//
+   int fd;		//Describe el archivo del FIFO
+   int end_process;	//Variable de fin del programa
+   int stringlen;	//Almacena la longitud de la cadena 
+   int read_bytes;	//indica el numero de bytes del FIFO
+   char readbuf[80];	//espacio para almacenar los datos 
+   char end_str[5];	//palabra para  terminar
 
    printf("FIFO_CLIENT: Send messages, infinitely, to end enter \"end\"\n");
 
@@ -30,20 +38,21 @@ int main() {//inicia el programa principal o main
 
       //printf("end_process is %d\n", end_process);
       if (end_process != 0) {
-	//
+         // Si la entrada no es "end", se envía la cadena al servidor a través del FIFO
          write(fd, readbuf, strlen(readbuf));
          printf("FIFOCLIENT: Sent string: \"%s\" and string length is %d\n", readbuf, (int)strlen(readbuf));
          read_bytes = read(fd, readbuf, sizeof(readbuf));
-	//
+	 // El cliente espera una respuesta del servidor
          readbuf[read_bytes] = '\0';
          printf("FIFOCLIENT: Received string: \"%s\" and length is %d\n", readbuf, (int)strlen(readbuf));
       } else {
-	//
+            // Si el usuario escribe "end", el cliente lo envía al servidor y finaliza
          write(fd, readbuf, strlen(readbuf));
          printf("FIFOCLIENT: Sent string: \"%s\" and string length is %d\n", readbuf, (int)strlen(readbuf));
          close(fd);
          break;
       }
    }
+//acaba el programa del cliente
    return 0;
 }
--- servidor.c
+++ servidor.c
@@ -1,3 +1,12 @@
+
+/**************************************************************************************
+ * Fecha: 14/10/2025
+ * Autor: Nicolas David Perdigon Zambrano
+ * Materia: Sistemas Operativos
+ * Tema: lab productor consumidor
+ * profesor: John Corredor
+ * ************************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,40 +16,47 @@
 //se utilizan librerias para que el programa funcione
 
 #define FIFO_FILE "/tmp/fifo_twoway"
-void reverse_string(char *);
-int main() {
-   int fd;
-   char readbuf[80];
-   char end[10];
-   int to_end;
-   int read_bytes;
 
-   /* Create the FIFO if it does not exist */
+//firma de la funcion para invertir el contenido del mensaje
+void reverse_string(char *);
+
+//prorgama principal
+int main() {
+   int fd; 		//describe el FIFO
+   char readbuf[80];	//espacio asignado para la recepcion de los datos
+   char end[10];	//cadena con la palabra clave para acabar
+   int to_end;		//controla la finalizacion del programa
+   int read_bytes;	//Numero de bytes desde el FIFO
+
+   /* crea el fifo si no existe */
    mkfifo(FIFO_FILE, S_IFIFO|0640);
    strcpy(end, "end");
+	//abre el FIFO de lectura y escritura
    fd = open(FIFO_FILE, O_RDWR);
    while(1) {
+	//el servidor lee los datos qie envio el cliente
       read_bytes = read(fd, readbuf, sizeof(readbuf));
       readbuf[read_bytes] = '\0';
       printf("FIFOSERVER: Received string: \"%s\" and length is %d\n", readbuf, (int)strlen(readbuf));
+      //si el mensaje es end, el servidor cierra la conexion
       to_end = strcmp(readbuf, end);
 
       if (to_end == 0) {
          close(fd);
          break;
       }
+	//el servidor revierte la cadena si el mensaje no es end
       reverse_string(readbuf);
       printf("FIFOSERVER: Sending Reversed String: \"%s\" and length is %d\n", readbuf, (int) strlen(readbuf));
-      write(fd, readbuf, strlen(readbuf));
-      /*
-      sleep - This is to make sure other process reads this, otherwise this
-      process would retrieve the message
-      */
+      //se envia la cadena invertida al cliente
+	write(fd, readbuf, strlen(readbuf));
+      //agrega una pausa para que el cliente reciba la informacion
       sleep(2);
    }
+	//acaba el programa principar para el servidor
    return 0;
 }
-
+//funcion para invertir el mensaje
 void reverse_string(char *str) {
    int last, limit, first;
    char temp;
